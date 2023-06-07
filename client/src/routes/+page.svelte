d49cf7cc53773318a8fb728f694a3bd6552b8a91
David Pescariu <davidpescariu12@gmail.com>
2023-06-07 15:46:05 +0300

    experiment: genezio init

---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { Hello } from "../../../server/sdk/hello.sdk";
+  const callGenezio = async () => {
+    const res = await Hello.sayHello();
+    alert(res);
+  }
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button on:click|preventDefault={callGenezio}>Test!</button>
